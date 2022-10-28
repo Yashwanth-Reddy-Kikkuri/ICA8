ca6452c9475ad80ba0c24d89151314256c168c41
Yashwanth-Reddy-Kikkuri <ykikkuri@asu.edu>
2022-10-27 20:13:07 -0700

    Fixed Fifth test case for count remaining

--- src/countRemaining.java
+++ src/countRemaining.java
@@ -5,7 +5,7 @@ public class countRemaining {
     public int countRemaining(String str) {
 
         int i,v = 0;
-        if (str.equals(null) || str.equals("")){
+        if (str == null || str.equals("")){
             return -1;
         }
 
