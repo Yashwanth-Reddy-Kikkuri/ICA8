53957d64fcc497d522dd5e9ed55e8aec2667be65
Yashwanth-Reddy-Kikkuri <ykikkuri@asu.edu>
2022-10-27 18:42:44 -0700

    Updated write to files

--- src/writeFile.java
+++ src/writeFile.java
@@ -1,4 +1,29 @@
+import java.io.*;
+
 public class writeFile {
-    public void writeToFile(StringBuilder output) {
+    private int number = 0;
+    public void writeToFile(StringBuilder output) throws IOException {
+        File f = fileName();
+        FileWriter fw = new FileWriter(f);
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(String.valueOf(output));
+        bw.close();
+    }
+
+    private File fileName(){
+        while(true) {
+            String filePath = "rule" + number + ".txt";
+            if(number == 0){
+                filePath = "rule.txt";
+            }
+            File f = new File(filePath);
+            if(f.exists()){
+                number++;
+            }
+            else{
+                number = 0;
+                return f;
+            }
+        }
     }
 }
